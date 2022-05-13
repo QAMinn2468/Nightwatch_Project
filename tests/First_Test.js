d3538e1d0d870c1a8849ae390d6dd0dcaf9e34f0
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2022-05-13 16:20:40 -0500

    Test 1: 3 PASS, Test 2: 2 PASS

--- tests/First_Test.js
+++ tests/First_Test.js
@@ -1,13 +1,80 @@
 module.exports = {
   'Verify Links Within Hamburger Menu'(browser) {
-    // navigate to new url:
+
     browser
-      .url('https://www.designory.com/')
-      .waitForElementVisible('#nav-toggle')
-      .click('#nav-toggle');
-      //.waitForElementVisible('.subnav')
-    // browser.assert.attributeContains(element, attribute, expected, [msg]);
-    // browser.assert.valueContains(element, expectedText, [msg]);
+      .url('https://www.designory.com/')     // navigate to new url
+      .waitForElementVisible('#nav-toggle')  // Hamburger icon is visable
+      .assert.not.visible('.subnav-toggle')  // location is not visible prior to clicking hamburger menu
+      .click('.navicon')                     // Click hamburger icon class='navicon'
+// ********************Navigation menu is open**********************************
+
+
+/*
+
+      verify ul contains the following li:
+      <a href="/work">WORK</a>
+      WORK = present
+
+      <a href="/about">ABOUT</a>
+      ABOUT = present
+
+      <a href="/careers">CAREERS</a>
+      CAREERS = present
+
+*/
+
+      .expect.element('.subnav-toggle').to.be.present  //      LOCATIONS = present
+
+
+
+/*
+      <a href="/contact">CONTACT</a>
+      CONTACT = present
+
+      <a href="/news">NEWS</a>
+      NEWS = present
+
+      ul contains 6 li = TRUE
+// expect.elements('li').count.to.equal(6)
+
+      &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+      Verify links open correct URL
+      &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//.waitForElementVisible('.subnav')
+
+      Within Div with class="nav-wrapper scroll"
+      verify:
+      WORK = https://www.designory.com/work
+      ABOUT = https://www.designory.com/about
+      CAREERS = https://www.designory.com/careers
+      LOCATIONS = https://www.designory.com/
+      ********************************************************
+      *with sublist containing: Long Beach, Chicago, Nashville, Tokyo, London, Hong Kong, Paris, New York, New Jersey, Portland, Philadelphia
+      */
+
+//      .assert.textContains('.subnav-toggle', 'LONG BEACH')
+
+/*
+      ul class="subnav"
+      contains all of the following:
+      <a href="/locations/long-beach">LONG BEACH</a>
+      <a href="/locations/chicago">CHICAGO</a>
+      <a href="/locations/nashville">NASHVILLE</a>
+      <a href="/locations/toyko">TOYKO</a>
+      <a href="/locations/london">LONDON</a>
+      <a href="/locations/hong kong">HONG KONG</a>
+      <a href="/locations/paris">PARIS</a>
+      <a href="/locations/new york">NEW YORK</a>
+      <a href="/locations/new jersey">NEW JERSEY</a>
+      <a href="/locations/portland">PORTLAND</a>
+      <a href="/locations/philadelphia">PHILADELPHIA</a>
+
+      ********************************************************
+      CONTACT = https://www.designory.com/contact
+      NEWS = https://www.designory.com/news
+
+      */
+
   }
 
 }
--- tests/Second_Test.js
+++ tests/Second_Test.js
@@ -4,8 +4,22 @@ module.exports = {
     browser
       .url('https://www.designory.com/locations/chicago')
       .waitForElementVisible('.location')
-    //  .click('#nav-toggle');
-    // browser.assert.attributeContains(element, attribute, expected, [msg]);
-    // browser.assert.valueContains(element, expectedText, [msg]);
+      .expect.element('.heading-xl').text.to.contain('CHI')       // H1 class="heading-xl" contains text "CHI"
+
+
+//      .verify.element('footer').text.to.contain('Chicago')      // Footer contains "Chicago"  (except website does NOT have Chicago here - EXPECT FAIL)
+//      .assert.cssProperty('h2', 'fontSize', '40px')             // font-size of H2 where class="heading-2" = 40px   (Font-size=1.75rem - EXPECT FAIL)
+//      .expect.element('img').to.have.attribute('src').equals('https://maps.googleapis.com/maps/api/staticmap?center= 225 N Michigan Ave, Suite 700 Chicago, IL 60601&zoom=16.5&markers= 225 N Michigan Ave, Suite 700 Chicago, IL 60601&size=400x150&key=AIzaSyAkAXQMgbxLGj6ZFqVAAR8JT4-5LaWKfIY')
+
+/*
+
+Verify:
+div class="side-section spacing-right" next p contains the following text "Phone: +1 312 729 4500"
+Anchor tag with class="location":     (nearly the same, pin in street vs pin inside block)
+img src = "https://maps.googleapis.com/maps/api/staticmap?center= 225 N Michigan Ave, Suite 700 Chicago, IL 60601&zoom=16.5&markers= 225 N Michigan Ave, Suite 700 Chicago, IL 60601&size=400x150&key=AIzaSyAkAXQMgbxLGj6ZFqVAAR8JT4-5LaWKfIY"
+
+*/
+
+
   }
 }
