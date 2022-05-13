114e897aa3f38c72468ce76d52bdca1911d31985
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2022-05-13 13:07:59 -0500

    both webpages open & both elements visible

--- tests/Second_Test.js
+++ tests/Second_Test.js
@@ -1,13 +1,11 @@
 module.exports = {
-  'Verify Links Within Hamburger Menu'(browser) {
+  'Verify Chicago Webpage'(browser) {
     // navigate to new url:
     browser
-      .url('https://www.designory.com/')
-      .waitForElementVisible('#nav-toggle')
-      .click('#nav-toggle');
-      //.waitForElementVisible('.subnav')
+      .url('https://www.designory.com/locations/chicago')
+      .waitForElementVisible('.location')
+    //  .click('#nav-toggle');
     // browser.assert.attributeContains(element, attribute, expected, [msg]);
     // browser.assert.valueContains(element, expectedText, [msg]);
   }
-
 }
